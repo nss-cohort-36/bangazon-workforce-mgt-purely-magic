051c7990c192532eb0a600bdbc51e124c73d0a43
Corri Golden <corri.golden@gmail.com>
2020-02-12 10:55:02 -0600

    finished list

--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -2,3 +2,4 @@ from .employees.employee_list import employee_list
 from .home import home
 from .auth.logout import logout_user
 from .connection import Connection
+from .training_program import training_program_list
