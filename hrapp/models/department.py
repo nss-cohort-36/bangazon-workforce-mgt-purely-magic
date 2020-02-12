bf44749e652cc27462d16caa11256d5b9c37fb9e
John Long <jlcomp45@gmail.com>
2020-02-11 20:05:59 -0600

    added comments to department.py, and created fixtures for departments, employees, training_programs

--- hrapp/models/department.py
+++ hrapp/models/department.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class Department(models.Model):
+    '''
+    description: This class creates a department and its properties
+    author: John Long
+    properties:
+      department_name: This property will contain the name of the department an employee will work in.
+      budget: This property contains the budget for the department in integer form so as to be usable for future math methods as needed.
+    '''
 
     department_name = models.CharField(max_length=50)
     budget = models.IntegerField(default=0)
