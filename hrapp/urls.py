415f2e0223a1763ffc45461749470ad269fa590f
Corri Golden <corri.golden@gmail.com>
2020-02-13 09:05:00 -0600

    update

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('departments/', department_list, name='departments'),
     path('computers/', computer_list, name='computers'),
     path('employees/form', employee_form, name='employee_form'),
+    path('training_program/form', training_program_form, name='training_program_form'),
 ]
