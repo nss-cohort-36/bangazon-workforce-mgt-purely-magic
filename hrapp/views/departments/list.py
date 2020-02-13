8b50adf3e154ca57205982ea95ecd41d74366e3d
John Long <jlcomp45@gmail.com>
2020-02-13 10:29:04 -0600

    corrected department list to show number of employees per dept

--- hrapp/views/departments/list.py
+++ hrapp/views/departments/list.py
@@ -1,14 +1,35 @@
 import sqlite3
 from django.shortcuts import render
-from hrapp.models import Department
+from hrapp.models import Department, Employee
 from ..connection import Connection
 from hrapp.models import model_factory
+
+def create_department(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    department = Department()
+    department.id = _row["department_id"]
+    department.name = _row["department_name"]
+    department.budget = _row["budget"]
+
+    department.employees = []
+
+    employee = Employee()
+    employee.id = _row["employee_id"]
+    employee.first_name = _row["first_name"]
+    employee.last_name = _row["last_name"]
+    employee.start_date = _row["start_date"]
+    employee.last_name = _row["last_name"]
+    employee.department_id = _row["department_id"]
+    employee.employee_id = _row["employee_id"]
+
+    return (department, employee)
 
 
 def department_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = model_factory(Department)
+            conn.row_factory = create_department
             db_cursor = conn.cursor()
 
             db_cursor.execute("""
@@ -19,16 +40,28 @@ def department_list(request):
                 e.first_name,
                 e.last_name,
                 e.start_date,
-                e.department_id
+                e.department_id,
+                e.id employee_id
             FROM hrapp_department d 
             JOIN hrapp_employee e ON d.id = e.department_id;
             """)
 
             all_departments = db_cursor.fetchall()
 
+            department_groups = {}
+
+            for (department, employee) in all_departments:
+
+                if department.id not in department_groups:
+                    department_groups[department.id] = department
+                    department_groups[department.id].employees.append(employee)
+
+                else:
+                    department_groups[department.id].employees.append(employee) 
+
         template = 'departments/list.html'
         context = {
-            'all_departments': all_departments
+            'all_departments': department_groups.values()
         }
 
         return render(request, template, context)
